3cb755f82db4b510fb17a1aa717d448b1ec2341b
James Whitmarsh <jwhitmarsh@dresources.com>
2019-12-13 10:32:46 +0000

    feat: export smoothScroll() method so it can be used programatically

--- src/index.js
+++ src/index.js
@@ -5,6 +5,47 @@ const getTop = function(element, start) {
   return element.getBoundingClientRect().top + start;
 };
 
+// we use requestAnimationFrame to be called by the browser before every repaint
+const requestAnimationFrame = window.requestAnimationFrame ||
+window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame ||
+function(fn) {
+  window.setTimeout(fn, 16);
+};
+
+export const smoothScroll = function({ scrollTo, offset = 0, duration = 500, container = window, updateHistory = true, hash }) {
+  // Using the history api to solve issue: back doesn't work
+  // most browser don't update :target when the history api is used:
+  // THIS IS A BUG FROM THE BROWSERS.
+  if (updateHistory && window.history.pushState && location.hash !== hash) window.history.pushState('', '', hash);
+
+  const startPoint = container.scrollTop || window.pageYOffset;
+  // Get the top position of an element in the document
+  // return value of html.getBoundingClientRect().top ... IE : 0, other browsers : -pageYOffset
+  let end = getTop(scrollTo, startPoint);
+
+  // Ajusts offset from the end
+  end += offset;
+
+  const clock = Date.now();
+  const step = function() {
+    // the time elapsed from the beginning of the scroll
+    const elapsed = Date.now() - clock;
+    // calculate the scroll position we should be in
+    let position = end;
+    if (elapsed < duration) {
+      position = startPoint + (end - startPoint) * easeInOutCubic(elapsed / duration);
+
+      requestAnimationFrame(step);
+    } else if (updateHistory) {
+      location.replace('#' + scrollTo.id);
+      // this will cause the :target to be activated.
+    }
+
+    container === window ? container.scrollTo(0, position) : (container.scrollTop = position);
+  };
+  step();
+}
+
 const VueSmoothScroll = {
   install(Vue, config) {
     Vue.directive('smooth-scroll', {
@@ -25,13 +66,6 @@ const VueSmoothScroll = {
           Object.assign(defaultValue, config);
         }
 
-        // we use requestAnimationFrame to be called by the browser before every repaint
-        const requestAnimationFrame = window.requestAnimationFrame ||
-        window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame ||
-        function(fn) {
-          window.setTimeout(fn, 16);
-        };
-
         let { duration, offset, container, updateHistory } = binding.value || {};
         duration = duration || defaultValue.duration;
         offset = offset || defaultValue.offset;
@@ -48,38 +82,7 @@ const VueSmoothScroll = {
           const scrollTo = document.getElementById(this.hash.substring(1));
           if (!scrollTo) return; // Do not scroll to non-existing node
 
-          // Using the history api to solve issue: back doesn't work
-          // most browser don't update :target when the history api is used:
-          // THIS IS A BUG FROM THE BROWSERS.
-          if (updateHistory && window.history.pushState && location.hash !== this.hash) window.history.pushState('', '', this.hash);
-
-
-          const startPoint = container.scrollTop || window.pageYOffset;
-          // Get the top position of an element in the document
-          // return value of html.getBoundingClientRect().top ... IE : 0, other browsers : -pageYOffset
-          let end = getTop(scrollTo, startPoint);
-
-          // Ajusts offset from the end
-          end += offset;
-
-          const clock = Date.now();
-          const step = function() {
-            // the time elapsed from the beginning of the scroll
-            const elapsed = Date.now() - clock;
-            // calculate the scroll position we should be in
-            let position = end;
-            if (elapsed < duration) {
-              position = startPoint + (end - startPoint) * easeInOutCubic(elapsed / duration);
-
-              requestAnimationFrame(step);
-            } else if (updateHistory) {
-              location.replace('#' + scrollTo.id);
-              // this will cause the :target to be activated.
-            }
-
-            container === window ? container.scrollTo(0, position) : (container.scrollTop = position);
-          };
-          step();
+          smoothScroll({ scrollTo, offset, duration, container, updateHistory, hash: this.hash });
         });
       }
     });
